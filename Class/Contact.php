34052addfbf09b0dd476af1d869f4c19eb18da62
Linyuu <sebastien.bayle124@gmail.com>
2015-06-22 14:34:32 +0200

    update

--- Class/Contact.php
+++ Class/Contact.php
@@ -2,5 +2,58 @@
 Function contact (){
 
 
+    $to = "sebastien.bayle124@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message sent from your site";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
+    $fields{"message"} = "message";
+
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
+
+<form id="contact" name="contact" method="post">
+    <fieldset>
+        <label for="name" id="name">Name<span class="required">*</span></label>
+        <input type="text" name="name" id="name" size="30" value="" required/>
+
+        <label for="email" id="email">Email<span class="required">*</span></label>
+        <input type="text" name="email" id="email" size="30" value="" required/>
+
+        <label for="phone" id="phone">Phone</label>
+        <input type="text" name="phone" id="phone" size="30" value="" />
+
+        <label for="Message" id="message">Message<span class="required">*</span></label>
+        <textarea name="message" id="message" required></textarea>
+
+        <label for="Captcha" id="captcha">Name the small house pet that says "<i>meow</i>"<span class="required">*</span></label>
+        <input type="text" name="captcha" value="" required/>
+
+        <input id="submit" type="submit" name="submit" value="Send" />
+    </fieldset>
+</form>
+
+<div id="success">
+  <span>
+    <p>Votre message a été correctement envoyé</p>
+  </span>
+</div>
+
+<div id="error">
+  <span>
+    <p>Quelque chose s'est mal passé, le message n'a pas été envoyé</p>
+  </span>
+</div>
+<?php
+
+
 }
 ?>
