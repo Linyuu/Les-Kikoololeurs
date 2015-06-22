d036c647cb8391cfcefb31a62767772bc89a83f3
Maxence GALLAND <maxence.galland@gmail.com>
2015-06-22 14:25:06 +0200

    variable de la class news

---
+++ Class/news.php
@@ -0,0 +1,20 @@
+<?php
+    class news{
+
+        /*
+         * titre de la news
+         */
+        private $_title;
+
+        /*
+         * Auteur de la news
+         */
+        private $_author;
+
+        /*
+         * contenu de la news
+         */
+        private $_content;
+
+    }
+?>
