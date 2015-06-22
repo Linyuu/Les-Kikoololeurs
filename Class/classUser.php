0b0ef3652e51dd2ec619b7b090a04ffc0e935537
Pierre-Yves Cognac <pierreyves.cognac@orange.fr>
2015-06-22 14:39:03 +0200

    Ajout des getters/setters

--- Class/classUser.php
+++ Class/classUser.php
@@ -1,6 +1,7 @@
 <?php
 	public class User
 	{
+		//Propriétés (variables)
 		private $nom;
 		
 		private $prenom
@@ -10,5 +11,76 @@
 		private $adresse
 		
 		private $email
+		
+		//Setters/Getters
+		public setnom($nom)
+		{
+			$this->nom = $nom;
+		}
+		
+		public getnom()
+		{
+			return $this->nom;
+		}
+		
+		public setprenom($prenom)
+		{
+			$this->prenom = $prenom;
+		}
+		
+		public getprenom()
+		{
+			return $this->prenom;
+		}
+		
+		public setage($age)
+		{
+			$this->age = $age;
+		}
+		
+		public getage()
+		{
+			return $this->age;
+		}
+		
+		public setadresse($adresse)
+		{
+			$this->adresse = $adresse;
+		}
+		
+		public getadresse()
+		{
+			return $this->adresse;
+		}
+		
+		public setemail($mail)
+		{
+			$this->prenom = $prenom;
+		}
+		
+		public getemail()
+		{
+			return $this->email;
+		}
+		
+		public setlogin($login)
+		{
+			$this->login = $login;
+		}
+		
+		public getlogin()
+		{
+			return $this->login;
+		}
+		
+		public setpassword($password)
+		{
+			$this->prenom = $password;
+		}
+		
+		public getpassword()
+		{
+			return $this->password;
+		}
 	}
 ?>
