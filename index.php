9221ac1e5613578a0d507250524b3de42916b6af
Linyuu <sebastien.bayle124@gmail.com>
2015-06-23 11:14:36 +0200

    upload

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Accueil <span class="sr-only">(current)</span></a></li>
                 <li><a href="inscription.php"><span class="glyphicon glyphicon-file"></span> S'inscrire</a></li>
                 <li><a href="connexion.php"><span class="glyphicon glyphicon-lock"></span> Login</a></li>
-                <li><a href="/Kikoo/Class/Contact.php"><span class="glyphicon glyphicon-magnet"></span> Contact</a></li>
+                <li><a href="<?php $contact=1?>"><span class="glyphicon glyphicon-magnet"></span> Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Link</a></li>
@@ -123,7 +123,6 @@
             <img src="images/facebook_logo.png" id="images_social_footer" title="Nous suivre sur Facebook !" alt="logo de facebook" /><img src="images/twitter_logo.png" id="images_social_footer" title="Nous suivre sur Twitter !" alt="logo de twitter" /><img src="images/google+_logo.png" id="images_social_footer" title="Nous suivre sur Google + !" alt="logo de google +" /><img src="images/rss_logo.png" id="images_social_footer" title="Nous suivre avec Flux RSS !" alt="logo de flux rss" />
         </div>
     </div>
-    <hr></hr>
     <footer>
         <p><span class="glyphicon glyphicon-copyright-mark"></span> Copyright IPHTEST 2015 <img src="images/logo_iphtest.png" id="footer_logo" alt="logo de iphtest" /></p>
     </footer>
@@ -169,6 +168,63 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
     else {
         $erreur = 'Au moins un des champs est vide.';
     }
+}
+if ($contact=1){
+    $to = "sebastien.bayle124@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message sent from your site";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
+    $fields{"message"} = "message";
+
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+
+    ?>
+
+    <form id="contact" name="contact" method="post">
+        <fieldset>
+            <label for="name" id="name">Name<span class="required">*</span></label>
+            <input type="text" name="name" id="name" size="30" value="" required/>
+
+            <label for="email" id="email">Email<span class="required">*</span></label>
+            <input type="text" name="email" id="email" size="30" value="" required/>
+
+            <label for="phone" id="phone">Phone</label>
+            <input type="text" name="phone" id="phone" size="30" value="" />
+
+            <label for="Message" id="message">Message<span class="required">*</span></label>
+            <textarea name="message" id="message" required></textarea>
+
+            <label for="Captcha" id="captcha">Name the small house pet that says "<i>meow</i>"<span class="required">*</span></label>
+            <input type="text" name="captcha" value="" required/>
+
+            <input id="submit" type="submit" name="submit" value="Send" />
+        </fieldset>
+    </form>
+
+    <div id="success">
+  <span>
+    <p>Votre message a été correctement envoyé</p>
+  </span>
+    </div>
+
+    <div id="error">
+  <span>
+    <p>Quelque chose s'est mal passé, le message n'a pas été envoyé</p>
+  </span>
+    </div>
+<?php
+
+
+}
+?>
 }
 ?>
 <head>
