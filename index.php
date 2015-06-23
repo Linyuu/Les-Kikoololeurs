ca18efd85f136f8841ef05786e6f42f3dd12c58e
Linyuu <sebastien.bayle124@gmail.com>
2015-06-23 11:38:37 +0200

    update index.php avec classement

--- index.php
+++ index.php
@@ -16,8 +16,6 @@
 
 <!-- Initialisation du java de bootstrap -->
 <script src="js/bootstrap.min.js"></script>
-
-<!-- Menu de navigation -->
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -55,6 +53,33 @@
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
+
+<!-- Partie PHP de connexion à l'espace membre fait par Sushi -->
+
+
+<?php
+// on teste si le visiteur a soumis le formulaire de connexion
+if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+    if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
+
+        $base = mysql_connect ('serveur', 'login', 'password');
+        mysql_select_db ('nom_base', $base);
+
+        // on teste si une entrée de BDD contient login / pass
+        $sql = 'SELECT count(*) FROM membre WHERE login="'.mysql_escape_string($_POST['login']).'" AND pass_md5="'.mysql_escape_string(md5($_POST['pass'])).'"';
+        $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+        $data = mysql_fetch_array($req);
+
+        mysql_free_result($req);
+        mysql_close();
+
+        // si réponse, alors l'utilisateur est un membre
+        if ($data[0] == 1) {
+            session_start();
+            $_SESSION['login'] = $_POST['login'];
+            header('Location: membre.php');
+            exit();
+            ?>
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -127,34 +152,7 @@
         <p><span class="glyphicon glyphicon-copyright-mark"></span> Copyright IPHTEST 2015 <img src="images/logo_iphtest.png" id="footer_logo" alt="logo de iphtest" /></p>
     </footer>
 </div>
-
-
-
-<!-- Partie PHP de connexion à l'espace membre fait par Sushi -->
-
-
 <?php
-// on teste si le visiteur a soumis le formulaire de connexion
-if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
-    if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
-
-        $base = mysql_connect ('serveur', 'login', 'password');
-        mysql_select_db ('nom_base', $base);
-
-        // on teste si une entrée de BDD contient login / pass
-        $sql = 'SELECT count(*) FROM membre WHERE login="'.mysql_escape_string($_POST['login']).'" AND pass_md5="'.mysql_escape_string(md5($_POST['pass'])).'"';
-        $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-        $data = mysql_fetch_array($req);
-
-        mysql_free_result($req);
-        mysql_close();
-
-        // si réponse, alors l'utilisateur est un membre
-        if ($data[0] == 1) {
-            session_start();
-            $_SESSION['login'] = $_POST['login'];
-            header('Location: membre.php');
-            exit();
         }
         // si aucune réponse, le visiteur s'est trompé soit dans son login, soit dans son mot de passe
         elseif ($data[0] == 0) {
@@ -211,34 +209,18 @@ if ($contact=1){
 
     <div id="success">
   <span>
-    <p>Votre message a été correctement envoyé</p>
+    Votre message a été correctement envoyé
   </span>
     </div>
 
     <div id="error">
   <span>
-    <p>Quelque chose s'est mal passé, le message n'a pas été envoyé</p>
+    Quelque chose s'est mal passé, le message n'a pas été envoyé
   </span>
     </div>
+
 <?php
-
-
 }
-?>
-}
-?>
-<head>
-    <title>Accueil</title>
-</head>
-
-Connexion à l'espace membre :<br />
-<form action="index.php" method="post">
-    Login : <input type="text" name="login" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>"><br />
-    Mot de passe : <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
-    <input type="submit" name="connexion" value="Connexion">
-</form>
-<a href="inscription.php">Vous inscrire</a>
-<?php
 if (isset($erreur)) echo '<br /><br />',$erreur;
 ?>
 </body>
